30b39ec91baf704640ac766f35e4ccf93c221212
Thu Yein Soe <thuyeinsoe@mabbank.com>
2024-10-28 14:23:13 +0630

    Resolved merge conflicts

---
+++ src/main/java/com/bmps/chat/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.bmps.chat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/bmps/chat/chat/MessageType.java
@@ -0,0 +1,12 @@
+package com.bmps.chat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
